9451ba0ca3df3e0b5d517d11e171091d0937c94d
Andi van der Putten <andi_vdputten@hotmail.com>
2026-09-18 00:14:44 +0200

    expanded dashboard testcases

--- tests/dashboard/dashboard.spec.ts
+++ tests/dashboard/dashboard.spec.ts
@@ -18,6 +18,7 @@ test ('DASH-001 Dashboard is visible after login',async ({page})=>{
 test('DASH-002 main navigation is available', async ({page})=>{
 const menuItems = [
     'Admin',
+    'PIM',
     'Leave',
     'Time',
     'Recruitment',
@@ -35,10 +36,59 @@ for (const menuItem of menuItems) {
         page.getByRole('link', { name: menuItem})
     ).toBeVisible();
 }
+});
+
+test('DASH-003 User dropdown menu can be opened', async ({page})=>{
+
+
+    await page.locator('span').filter({ hasText: 'manda user' }).click();
+    await page.getByRole('menuitem', { name: 'About' }).click();
+
+    await expect (
+        page.getByRole('heading', { name: 'About' })
+    ).toBeVisible();
+
+});
+
+test ("DASH-004 My actions are available", async ({page})=>{
+   await expect(
+    page.locator('.orangehrm-dashboard-widget-name').filter({hasText: 'My Actions'})
+    ).toBeVisible();
+
+   await expect (
+    page.locator('button.oxd-icon-button--danger')
+   ).toBeVisible();
+
+   await expect(
+    page.locator('button.oxd-icon-button--info')
+   ).toBeVisible();
+
+   await expect(
+    page.locator('.orangehrm-todo-list-item')
+   ).not.toHaveCount(0);    
+});
+
+test ('DASH-005 Dashboard widgets are available', async ({page})=>{
+    const dasboardWidgetItems =[
+        'Time at Work',
+        'My Actions',
+        'Quick Launch',
+        'Buzz Latest Posts',
+        'Employees on Leave Today',
+        'Employee Distribution by Sub Unit',
+        'Employee Distribution by Location',
+    
+    ];
+
+for (const dashboardWidgetItem of dasboardWidgetItems) {
+    await expect(
+        page.locator('.orangehrm-dashboard-widget-name').filter({hasText : dashboardWidgetItem})
+    ).toBeVisible();
+};
 })
 
-test('DASH-003 Userprofile can be opened', async ({page})=>{
-const aboutMenuItems = [
+test ('DASH-008 About information can be opened from user menu', async ({page})=>{
+    const aboutMenuItems = [
     'Company Name',
     'Version',
     'Active Employees',
@@ -46,14 +96,7 @@ const aboutMenuItems = [
     'Terminated',
 ];
 
-    await page.locator('span').filter({ hasText: 'manda user' }).click();
-    await page.getByRole('menuitem', { name: 'About' }).click();
-
-    await expect (
-        await page.getByRole('heading', { name: 'About' })
-    ).toBeVisible();
-
-    for (const aboutMenuItem of aboutMenuItems) {
+for (const aboutMenuItem of aboutMenuItems) {
     await expect (
         page.locator('.oxd-grid-2').filter({hasText: aboutMenuItem})
     ).toBeVisible();
@@ -61,5 +104,4 @@ const aboutMenuItems = [
     await expect(
         page.getByRole('dialog').getByText('OrangeHRM', {exact: true})
     ).toBeVisible();
-
 })
